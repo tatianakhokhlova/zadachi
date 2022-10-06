b328d3234610b94f659415878661292cb4651df5
Tatyana <tatyana.dovganskaya@gmail.com>
2022-10-06 13:59:52 +0300

    задача 3 поиск максимального числа

---
+++ 003/Program.cs
@@ -0,0 +1,21 @@
+﻿Console.WriteLine("Введите первое число");
+int a = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите второе число");
+int b = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите третье число");
+int c = int.Parse(Console.ReadLine());
+int max = a;
+
+    if(max < b)
+    {
+        max = b;
+    }
+    else
+    {
+        if(max < c)
+        {
+          max = c;  
+        }
+    }
+Console.Write("Максимальное число:  ");
+Console.WriteLine(max);

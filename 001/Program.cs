a60eabdb2bf5436cf87a38b689cc4e2fe21fb55c
Tatyana <tatyana.dovganskaya@gmail.com>
2022-10-07 12:16:56 +0300

    задача 001

---
+++ 001/Program.cs
@@ -0,0 +1,7 @@
+﻿int A = new Random().Next(1, 50);
+int B = new Random().Next(1, 10);
+if (B*B==A)
+Console.WriteLine($"Число {A} квадрат числа {B}");
+else Console.WriteLine($"Число {A} не квадрат числа {B}");
+
+Console.WriteLine();

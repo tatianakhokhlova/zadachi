6c4543963b8d36817acdb9a5bb0f6c259478ea21
Tatyana <tatyana.dovganskaya@gmail.com>
2022-10-07 12:18:36 +0300

    задача 002

---
+++ 002/Program.cs
@@ -0,0 +1,40 @@
+﻿Console.Clear();
+
+int a;
+Console.WriteLine("Введите номер дня недели : ");
+a = int.Parse(Console.ReadLine() ?? "0");
+{
+    if(a == 1)
+    {
+        Console.WriteLine("Понедельник");
+    }
+    else
+    if(a == 2)
+    {
+        Console.WriteLine("Вторник");
+    }
+    else
+    if(a == 3)
+    {
+        Console.WriteLine("Среда");
+    }
+    else
+    if(a == 4)
+    {
+        Console.WriteLine("Четверг");
+    }
+    else
+    if(a == 5)
+    {
+        Console.WriteLine("Пятница");
+    }
+    else
+    if(a == 6)
+    {
+        Console.WriteLine("Суббота");
+    }
+    else
+    if(a == 7)
+    {
+        Console.WriteLine("Воскресенье");
+}}

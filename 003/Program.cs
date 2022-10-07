af4550e2d60a18a56ddea9fd2fa67629ef9d2459
tatianakhokhlova <114282908+tatianakhokhlova@users.noreply.github.com>
2022-10-07 12:23:12 +0300

    Update Program.cs
    Найти максимальное из трех чисел

--- 003/Program.cs
+++ 003/Program.cs
@@ -1,4 +1,6 @@
-﻿Console.WriteLine("Введите первое число");
+// Найти максимальное из трех чисел
+
+Console.WriteLine("Введите первое число");
 int a = int.Parse(Console.ReadLine());
 Console.WriteLine("Введите второе число");
 int b = int.Parse(Console.ReadLine());

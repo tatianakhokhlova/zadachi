bf0b1d96bbc8257a49cbf06563b274fe58105ac1
Tatyana <tatyana.dovganskaya@gmail.com>
2022-10-07 11:51:56 +0300

    Задача010

---
+++ 010/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine ("ВВедите первое число");
+string str = Console.ReadLine();
+int a = Convert.ToInt32(str);
+Console.WriteLine ("ВВедите второе число");
+str = Console.ReadLine();
+int b = Convert.ToInt32(str);
+
+if(a % b == 0) {
+    Console.WriteLine("Число " + a + " кратно числу " + b);
+} else {
+    Console.WriteLine("Число " + a + " не кратно числу " + b);
+}

4f4eab8e29a144794552f2878589c52212e83f9c
Tatyana <tatyana.dovganskaya@gmail.com>
2022-10-07 11:50:51 +0300

    Задача005

---
+++ 005/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Введите число N");
+string n = Console.ReadLine();
+int N = Convert.ToInt32(n);
+
+int counter = 1;
+
+while(counter <= N)
+{
+    if(counter % 2 == 0) {
+        Console.WriteLine(counter);
+    } 
+    counter = counter + 1;
+}

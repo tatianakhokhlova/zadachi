a83f3285aa8f0cad7c5c07dd828cab1d22b8c480
Tatyana <tatyana.dovganskaya@gmail.com>
2022-10-07 12:35:30 +0300

    задача 011

---
+++ 011/Program.cs
@@ -0,0 +1,8 @@
+﻿int Number  = new Random().Next(1, 5000);
+System.Console.WriteLine();
+System.Console.WriteLine($"число: {Number}");
+
+if (Number / 100 > 0) Console.WriteLine($"Третьей с конца цифрой числа {Number} является цифра {Number / 100 % 10}");
+else Console.WriteLine($"У числа {Number} нет третьей цифры");
+
+Console.WriteLine();

cf6bfb0f4c6ed26f08c8ccd6edcbf42f3f52ed0d
Tatyana <tatyana.dovganskaya@gmail.com>
2022-10-07 11:51:31 +0300

    Задача008

---
+++ 008/Program.cs
@@ -0,0 +1,21 @@
+﻿int number = new Random().Next(10, 99);
+int max = 0;
+int maxDigit()
+{
+    int firstDigit = number / 10;
+    int secondDigit = number % 10;
+    if (firstDigit > secondDigit)
+    {
+        max = firstDigit;
+    }
+    else
+    {
+        max = secondDigit;
+    }
+
+    return max;
+}
+maxDigit();
+Console.WriteLine(number);
+Console.Write("Наибольшая цифра числа: ");
+Console.WriteLine(max);

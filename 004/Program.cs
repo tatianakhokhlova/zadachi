4b0423ee900bd8cf5a2405b90cedc08b70029d59
Tatyana <tatyana.dovganskaya@gmail.com>
2022-10-07 11:50:25 +0300

    задача4

---
+++ 004/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Clear();
+
+int a;
+Console.WriteLine("Введите число: ");
+a = int.Parse(Console.ReadLine() ?? "0");
+if (a % 2 == 0)
+{
+    Console.WriteLine("Число четное");
+}
+else
+{
+    Console.WriteLine("Число нечетное");
+}
+Console.WriteLine();
